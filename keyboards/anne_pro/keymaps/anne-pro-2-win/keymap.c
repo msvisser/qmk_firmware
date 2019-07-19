f129af31cdb490a2a75cdb69de53b935ad2a1c24
angelokofficial <angelok2015.mc@gmail.com>
2019-07-20 00:21:05 +0300

    Upload

--- keyboards/anne_pro/keymaps/anne-pro-2-win/keymap.c
+++ keyboards/anne_pro/keymaps/anne-pro-2-win/keymap.c
@@ -16,14 +16,16 @@
 
 #include QMK_KEYBOARD_H
 
-/* Default Anne Pro layout, arrows style */
+/* Anne Pro 2 default layout
+Includes Media Keys and TAP features, see readme.md */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                     MT(MOD_RSFT, KC_UP),
-      KC_LCTL, KC_LWIN,   KC_LALT,                   KC_SPACE,                                    KC_LALT, LT(1,KC_LEFT), LT(2,KC_DOWN), MT(MOD_RCTL, KC_RGHT)
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,	MT(MOD_RSFT, KC_UP),
+      KC_LCTL, KC_LGUI,   KC_LALT,                   KC_SPACE,                     KC_LALT, LT(1,KC_LEFT), LT(2,KC_DOWN), MT(MOD_RCTL, KC_RGHT)
   ),
   [1] = LAYOUT(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
@@ -33,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______,                   XXXXXXX,                                     _______, _______, _______, _______
   ),
   [2] = LAYOUT(
-      XXXXXXX,   KC_MSTP, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, XXXXXXX, APL_MOD, APL_RGB, APL_RAT, APL_BRT, XXXXXXX,
-      RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RESET,   KC_MSTP, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, XXXXXXX, APL_MOD, APL_RGB, APL_RAT, APL_BRT, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   _______,
       _______, _______, _______,                   XXXXXXX,                                     _______, _______, _______, _______
--- keyboards/anne_pro/keymaps/anne-pro-2-win/readme.md
+++ keyboards/anne_pro/keymaps/anne-pro-2-win/readme.md
@@ -1,36 +1,60 @@
-This keymap was made with love to make Anne Pro 2 keys style. Supports Windows, some os-specific features can not work with different os like OSX.
-Keymap will be synced with all changes which dear msvisser will make.
+# Anne Pro 2 Windows layout
 
-Keys:
+This layout adds TAP, Media Keys features and AP2-like backlight control. 
+
+*Keys:*
+
 ANNE+0 - off/on the backlight
-ANNE+9 - switch the backlight profile
-ANNE + =/+ - increases brightness
-ANNE + - -/_ - changes backlight speed
-ANNE+TAB - reset the keyboard (instead of button on the back side)
-ANNE+TAB+hold ESC - enter to the bootloader (DFU) mode
 
-TAP:
+ANNE+9 - switch the backlight profile
+
+ANNE + =/+ - increases brightness
+
+ANNE + - -/_ - changes backlight speed
+
+ANNE+ESC - reset the keyboard (instead of button on the back side)
+
+ANNE+ESC+hold ESC - enter to the bootloader (DFU) mode
+
+*TAP:*
+
 Right Shift - up arrow
+
 ANNE - down arrow
+
 FN - left arrow
+
 Right CTRL - right arrow 
 
-Bluetooth:
+*Bluetooth:*
+
 Fn+B - enter BT mode
+
 =/+ - turn ON BT
+
 -/_ - turn OFF BT
+
 0 - enter legacy BT mode
+
 1,2,3,4 - should work as bt profiles for switch among connected devices
 
-Media keys:
+*Media keys:*
+
 ANNE+1 - stop
+
 ANNE+2 - volume down
+
 ANNE+3 - volume up
+
 ANNE+4 - previous track
+
 ANNE+5 - pause/play
+
 ANNE+6 - next track
+
 ANNE+7 - mute/unmute volume
 
 
-All other keys (like del, ins, pause, prnt.scr, home, etc) remain on the inital AP1 places.
-To compile: `make anne_pro:ap2style_win`
+All other keys remain on their own places.
+
+To compile: `make anne_pro:<your_keymap>`
